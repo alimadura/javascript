83f70ceffff9a19672f069b4a522c4067a403138
alimadura <alimadura@alumni.undip.ac.id>
2016-10-23 15:14:30 +0700

    Update ruceh.js

--- ruceh.js
+++ ruceh.js
@@ -1,1 +1,1 @@
-document.write("<style type=text/css>div#alimadura{top:0;position:fixed;width:100%;opacity:.0;center:0}div#alimadura img{border:0;cursor:pointer}</style><div align=center id=alimadura style=height:280px;z-index:9999>");
+document.write("<style type=text/css>div#alimadura{top:0;position:fixed;width:100%;opacity:.0;center:0}div#alimadura img{border:0;cursor:pointer}</style><div align=center id=alimadura style=height:250px;z-index:9999>");
